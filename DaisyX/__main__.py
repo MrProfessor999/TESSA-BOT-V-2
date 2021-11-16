6f18bd9438495926e1ec4ea8ba4a27a963e63bfa
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-16 15:47:22 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -226,7 +226,7 @@ def start(update: Update, context: CallbackContext):
     ],
 ]
 
-            )
+            
     else:
         update.effective_message.reply_text(
             "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
