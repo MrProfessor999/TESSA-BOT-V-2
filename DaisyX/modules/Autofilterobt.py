a431889196b4b32b0c9adf12faaca81ace33a5d0
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-17 12:36:30 +0530

    Create Autofilterobt.py

---
+++ DaisyX/modules/Autofilterobt.py
@@ -0,0 +1,10 @@
+__help__ = """
+ Help: Auto Filter
+
+NOTE:
+1. Make me the admin of your channel if it's private.
+2. make sure that your channel does not contains cam rip, porn and fake files.
+3. Forward the last message to me with quotes.
+ I'll add all the files in that channel to my db.
+"""
+__mod_name__ = "💫AUTOFILTER💫"
