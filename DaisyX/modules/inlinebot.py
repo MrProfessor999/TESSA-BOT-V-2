7375fdc62699e0992ba1b470237ba9e3e07cb6c2
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-22 09:33:24 +0530

    Update inlinebot.py

--- DaisyX/modules/inlinebot.py
+++ DaisyX/modules/inlinebot.py
@@ -86,12 +86,12 @@ class AioHttp:
 
 __mod_name__ = "Inline"
 __help__ = """
- <b> INLINE BOT SERVICE OF @DAISYXBOT </b> 
+ <b> INLINE BOT SERVICE OF @TESSA_DOG_BOT </b> 
  
 <i> I'm more efficient when added as group admin. By the way these commands can be used by anyone in a group via inline.</i>
 
 <b>Syntax</b>
-   @DaisyXBot [command] [query]
+   @TESSA_DOG_BOT [command] [query]
 
 <b> Commands Available</b>
 - alive - Check Bot's Stats.
@@ -131,11 +131,11 @@ __help__ = """
 
 __MODULE__ = "Inline"
 __HELP__ = """
- ==>> **INLINE BOT SERVICE OF @DAISYXBOT** <<==
+ ==>> **INLINE BOT SERVICE OF @TESSA_DOG_BOT** <<==
 `I'm more efficient when added as group admin. By the way these commands can be used by anyone in a group via inline.`
 
    >> Syntax <<
-@DaisyXBot [command] [query]
+@TESSA_DOG_BOT [command] [query]
 
    >> Commands Available <<
 - **alive** - __Check Bot's Stats.__
